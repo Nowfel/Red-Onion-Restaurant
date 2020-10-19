a3fb5a3860f5b6840a8de98995939c1511a5cfef
Nowfel <53569184+Nowfel@users.noreply.github.com>
2020-10-19 11:27:46 +0600

    context and auth added

---
+++ src/components/useAuth/useAuth.js
@@ -0,0 +1,50 @@
+import React from "react";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { useState } from "react";
+import { useEffect } from "react";
+import firebaseConfig from "../../firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+
+const getUser = (user) => {
+  const { displayName, email, photoURL } = user;
+  return { name: displayName, email, photo: photoURL };
+};
+const Auth = () => {
+  const [user, setUser] = useState(null);
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        const signedInUser = getUser(res.user);
+        //const {displayName, email, photoURL} = res.user;
+        //const signedInUser={name: displayName, email,photo: //photoURL};
+        setUser(signedInUser);
+        return res.user;
+      })
+      .catch((err) => {
+        console.log(err);
+        setUser(null);
+        return err.message;
+      });
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        const currentUser = getUser(user);
+        setUser(currentUser);
+      } else {
+      }
+    });
+  }, []);
+  return {
+    signInWithGoogle,
+    user,
+  };
+};
+export default Auth;
